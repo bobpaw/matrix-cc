76af803c84b8511f0a13a4389430636fd6371c1c
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-10-26 10:57:02 -0500

    Added tests for container
    container.cc is test for class in container.h

---
+++ container.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "container.h"
+
+int main (int argc, char *argv[]) {
+std::string line;
+	try {
+	while (true) {
+	  std::cout << "> " << std::flush;
+	  std::getline(std::cin, line);
+	  if (line.size() == 0 || std::string("exit").find(line) == 0 || std::string("quit").find(line) == 0) break;
+	  container::TextHolder a(line, argc > 1 ? "[]" : "()");
+	  std::cout << a.toFunString() << std::flush;
+	  }
+	} catch (const char *e) {
+	  std::cerr << e << std::endl;
+	  return -1;
+	}
+	if (std::cin.eof()) std::cout << std::endl << std::flush;
+	return 0;
+}
