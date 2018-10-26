bc476393f12e770fa3181f61a4a15d75fde3f7b3
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-10-26 10:56:30 -0500

    Add text to show which is which

--- matrix.cc
+++ matrix.cc
@@ -6,9 +6,10 @@ int main () {
 	try {
 		auto c = matrix::Matrix<>({{2, 3, 0, 4},{2, 3, 1, -1},{4, 7, 1, 2},{3, 5, 3, 0}});
 		auto a = matrix::I(c.rows());
-		std::cout << c << std::endl;
-		std::cout << a << std::endl;
-		std::cout << c * a << std::endl;
+		std::cout << "c: " << c << std::endl;
+		std::cout << "a: " << a << std::endl;
+		std::cout << "c * a: " << c * a << std::endl;
+		std::cout << "c * 4: " << c * 4 << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 		return -1;
